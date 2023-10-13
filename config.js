ff68f997ccaa701dc1301b95776cae7e347cf1e3
MinhToan4 <87869289+MinhToan4@users.noreply.github.com>
2023-10-14 00:08:55 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Hà à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao Hà cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi!',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, ANH TRAI à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Giữ gìn sức khỏe lun đáng iu nhá!',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100032719290770' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
